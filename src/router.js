8fd925ac9be4d0fde7c4b545c0d6632044b8c460
johnnywwy <johnnywwy@gmail.com>
2022-07-01 19:25:22 +0800

    抽离路由到common\config\router.js 并使用重定向

--- src/router.js
+++ src/router.js
@@ -1,29 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import layout from './views/layout'
+import routes from './common/config/router'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'layout',
-      component: layout,
-      children: [
-        {
-          path: '/index',
-          name: 'index',
-          component: () => import('./views/index/index'),
-        }
-      ]
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('./views/login/index.vue'),
-
-    }
-  ]
+  routes
 })
