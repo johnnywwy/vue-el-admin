be2da857b37a2af32cb859cc03780c9731e8ca87
johnnywwy <johnnywwy@gmail.com>
2022-07-01 23:36:24 +0800

    引入bootstrap

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -1,3 +1,10 @@
+/*
+*规则：
+* 一、例如：index/index, shop/index 以index结尾的，path和name默认去除idnex
+* 二、例如：shop/list, 默认生成的name为shop_list (如果结尾为index，例如shop/index 则是shop)
+* 三、填写后不会自动生成
+*/
+
 let routes = [
   {
     path: '/',
@@ -11,6 +18,11 @@ let routes = [
         name: 'index',
         // component: () => import('../../views/index/index.vue'),
         component: 'index/index',
+      },
+      {
+        path: '/shop/goods/list',
+        name: 'shop_goods_list',
+        component: 'shop/goods/list',
       }
     ]
   },
@@ -38,6 +50,13 @@ function createRoute(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (!arr[i].component) return
 
+    //生成name
+    let val = getValue(arr[i].name)
+    arr[i].name = arr[i].name || val.replace(/\//g, '_')
+
+    //生成path
+    arr[i].path = arr[i].path || `/${val}`
+
     //自动生成component
     let componentFn = import(`../../views/${arr[i].component}.vue`)
     arr[i].component = () => componentFn
@@ -47,7 +66,23 @@ function createRoute(arr) {
       createRoute(arr[i].children)
     }
   }
+}
 
+//去除index
+function getValue(str) {
+  // str = 'login/index'
+  //获取最后一个/ 的索引
+  let index = str.lastIndexOf('/')
+  // console.log(str, index)
+
+  //获取最后一个/ 到末尾的字符串
+  let val = str.substring(index + 1, str.length)
+  // console.log(val, val)
+  //判断是不是index结尾
+  if (val === 'index') {
+    return str.substring(index, -1)
+  }
+  return str
 }
 
 export default getRoutes()
---
+++ src/views/shop/goods/list.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    商品列表
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list'
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
