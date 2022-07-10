a8368d95a50bbc4acf564a017eedb55160401eee
johnnywwy <johnnywwy@gmail.com>
2022-07-10 15:03:04 +0800

    使用插槽封装button-search

---
+++ src/components/common/button-search.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="tab-content">
+      <!--左边按钮-->
+      <slot name="left"></slot>
+      <!--右边按钮-->
+      <div class="ml-auto" v-show="!superSearch">
+        <slot name="right">
+          <el-input v-model="keyword" size="mini"
+                    :placeholder="placeholder" class="input">
+          </el-input>
+          <el-button type="info" size="mini" @click="$emit('search',keyword)">搜索</el-button>
+          <el-button size="mini" @click="superSearch=true">高级搜索</el-button>
+        </slot>
+      </div>
+    </div>
+    <el-card class="card-wrapper box-card bg-light my-3" v-show="superSearch">
+      <div slot="header" class="clearfix header">
+        <span>高级搜索</span>
+        <el-button style="float: right; padding: 3px 0" type="text"
+                   @click="closeSuperSearch">收起
+        </el-button>
+      </div>
+      <!--表单-->
+      <slot name="form"></slot>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'button-search',
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      keyword: '',
+      superSearch: false,
+    }
+  },
+  methods: {
+    //高级搜索的显示与隐藏
+    closeSuperSearch() {
+      this.superSearch = false
+    },
+  }
+
+};
+</script>
+
+<style lang="scss">
+.tab-content {
+  display: flex;
+  align-items: center;
+
+  .input {
+    margin-right: 1rem;
+    width: 150px;
+  }
+}
+
+.card-wrapper {
+  .header {
+    margin: -10px;
+  }
+
+  .el-form {
+    .el-form-item {
+      .el-input {
+        width: 200px;
+      }
+
+      .el-select {
+        width: 200px;
+      }
+    }
+
+
+  }
+}
+
+</style>
--- src/views/shop/goods/list.vue
+++ src/views/shop/goods/list.vue
@@ -1,62 +1,54 @@
 <template>
   <div class="wrapper">
-    <el-tabs class="el-tabs-wrapper" v-model="tabIndex"
+    <el-tabs class="el-tabs-wrapper"
+             v-model="tabIndex"
              @tab-click="handleClick">
       <el-tab-pane :label="tab.name" :key="tabIndex"
                    v-for="(tab,tabIndex) in tabBars">
-        <div class="tab-content">
-          <!--左边按钮-->
-          <el-button type="success" size="mini">发布商品</el-button>
-          <el-button type="danger" size="mini">批量删除</el-button>
-          <!--右边按钮-->
-          <div class="ml-auto" v-show="!superSearch">
-            <el-input v-model="form.name" size="mini"
-                      placeholder="要搜索的商品名称" class="input">
-
-            </el-input>
-            <el-button type="info" size="mini">搜索</el-button>
-            <el-button size="mini" @click="toggleSearch">高级搜索</el-button>
-          </div>
-        </div>
-
-        <el-card class="card-wrapper box-card bg-light my-3" v-show="superSearch">
-          <div slot="header" class="clearfix header">
-            <span>高级搜索</span>
-            <el-button style="float: right; padding: 3px 0" type="text" @click="toggleSearch">收起</el-button>
-          </div>
-          <!--表单-->
-          <el-form :inline="true" :model="form" class="demo-form-inline">
-            <el-form-item label="商品名称" class="mb-0">
-              <el-input v-model="form.name" placeholder="请输入商品名称" size="mini"></el-input>
-            </el-form-item>
-            <el-form-item label="商品编码" class="mb-0">
-              <el-input v-model="form.code" placeholder="请输入商品编码" size="mini"></el-input>
-            </el-form-item>
-            <el-form-item label="商品类型" class="mb-0">
-              <el-select v-model="form.type" placeholder="活动区域" size="mini">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="商品分类" class="mb-0">
-              <el-input v-model="form.category" placeholder="请输入商品分类" size="mini">
-              </el-input>
-            </el-form-item>
-
-            <el-form-item class="mb-0">
-              <el-button type="info" @click="search" size="mini">搜索</el-button>
-              <el-button type="danger" size="mini" @click="clearAllSearch">清空删选条件</el-button>
-            </el-form-item>
-          </el-form>
-        </el-card>
-
+        <button-search placeholder="要搜索的商品名称" ref="buttonSearch" @search="search">
+          <template #left>
+            <!--左边按钮-->
+            <el-button type="success" size="mini">发布商品</el-button>
+            <el-button type="danger" size="mini">批量删除</el-button>
+          </template>
+          <!--高级搜索表单-->
+          <template #form>
+            <el-form :inline="true" :model="form" class="demo-form-inline">
+              <el-form-item label="商品名称" class="mb-0">
+                <el-input v-model="form.name" placeholder="请输入商品名称" size="mini"></el-input>
+              </el-form-item>
+              <el-form-item label="商品编码" class="mb-0">
+                <el-input v-model="form.code" placeholder="请输入商品编码" size="mini"></el-input>
+              </el-form-item>
+              <el-form-item label="商品类型" class="mb-0">
+                <el-select v-model="form.type" placeholder="活动区域" size="mini">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="商品分类" class="mb-0">
+                <el-input v-model="form.category" placeholder="请输入商品分类" size="mini">
+                </el-input>
+              </el-form-item>
+              <el-form-item class="mb-0">
+                <el-button type="info" @click="search" size="mini">搜索</el-button>
+                <el-button type="danger" size="mini" @click="clearAllSearch">清空删选条件</el-button>
+              </el-form-item>
+            </el-form>
+          </template>
+        </button-search>
       </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script>
+import buttonSearch from '../../../components/common/button-search'
+
 export default {
+  components: {
+    buttonSearch
+  },
   name: 'list',
   data() {
     return {
@@ -74,8 +66,6 @@ export default {
         type: '',
         category: ''
       },
-      superSearch: false,
-
     }
   },
   methods: {
@@ -83,13 +73,15 @@ export default {
     handleClick(tab) {
       console.log('tab.index', tab.index)
     },
-    //高级搜索的显示与隐藏
-    toggleSearch() {
-      this.superSearch = !this.superSearch
-    },
     //提交高级搜索
-    search() {
-      console.log('搜索中啦')
+    search(e = false) {
+      //简单搜索
+      if (typeof e === 'string') {
+        return console.log('简单搜索', e)
+      }
+      //高级搜索
+      console.log('高级搜索')
+
     },
     //  清空所有
     clearAllSearch() {
@@ -99,7 +91,8 @@ export default {
         type: '',
         category: ''
       }
-      // this.toggleSearch()
+      //点击清空所有 同时关闭高级搜索
+      this.$refs.buttonSearch[this.tabIndex].closeSuperSearch()
     }
   }
 
@@ -132,9 +125,6 @@ export default {
       }
 
       .el-form {
-
-        border: 1px solid red;
-
         .el-form-item {
           .el-input {
             width: 200px;
